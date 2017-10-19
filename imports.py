488f98dba4923fac4376e3cf17574cb44983adb0
calvinhklui <calvinhklui@yahoo.com>
2017-10-19 01:39:08 -0400

    renamed audit data and experimented with sk-learn's nnmf

--- imports.py
+++ imports.py
@@ -4,7 +4,7 @@ from calParser import obtainSchedule
 from audit_parser import audit_info
 from lsa_recommender import export_to_master,filter_available_classes
 from decision_tree import preference_score,top_preferred_courses
-from recommender_system import loadAudits, inputData, buildRecommender, makePrediction, compileDepartScores
+from collaborative_filtering import loadAudits, inputData, buildRecommender, makePrediction, compileDepartScores
 from time import time
 import json
 
